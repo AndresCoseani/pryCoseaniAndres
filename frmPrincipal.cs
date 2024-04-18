671e23d084a29c3ecdcda4abf59793acbd39c99e
Andres Coseani <andycoseani.16@gmail.com>
2024-04-18 19:02:51 -0300

    Firma

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -7,10 +7,6 @@ namespace ProyectoCoseaniAndres
             InitializeComponent();
         }
 
-        private void firmaToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
-            frmFirma f = new frmFirma();
-            f.ShowDialog();
-        }
+     
     }
 }
