5c20eb65cb4f2c629c111501c21dcb441279ba8f
Andres Coseani <andycoseani.16@gmail.com>
2024-04-18 18:58:34 -0300

    Firma

---
+++ frmFirma.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ProyectoCoseaniAndres
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picFirma = new PictureBox();
+            btnGuardar = new Button();
+            ((System.ComponentModel.ISupportInitialize)picFirma).BeginInit();
+            SuspendLayout();
+            // 
+            // picFirma
+            // 
+            picFirma.Location = new Point(13, 13);
+            picFirma.Name = "picFirma";
+            picFirma.Size = new Size(772, 388);
+            picFirma.TabIndex = 0;
+            picFirma.TabStop = false;
+            picFirma.MouseMove += picFirma_MouseMove;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(356, 407);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(111, 31);
+            btnGuardar.TabIndex = 1;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // frmFirma
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnGuardar);
+            Controls.Add(picFirma);
+            Name = "frmFirma";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmFirma";
+            ((System.ComponentModel.ISupportInitialize)picFirma).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox picFirma;
+        private Button btnGuardar;
+    }
+}
---
+++ frmFirma.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCoseaniAndres
+{
+    public partial class frmFirma : Form
+    {
+        private Bitmap ArchivoImagen;
+        public frmFirma()
+        {
+            InitializeComponent();
+            ArchivoImagen = new Bitmap(picFirma.Width, picFirma.Height);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            DateTime fechaHoraActual = DateTime.Now;
+            string nombreArchivo = $"Firma_{fechaHoraActual:yyyy.MM.dd},{fechaHoraActual:HH.mm}hrs.jpg";
+            string carpetaFirma = @"C:\Users\andyc\source\repos\pryCoseaniAndresI\bin\Debug\net6.0-windows\FIRMAS";
+            string rutaFirma = Path.Combine(carpetaFirma, nombreArchivo);
+            ArchivoImagen.Save(rutaFirma, System.Drawing.Imaging.ImageFormat.Jpeg);
+            MessageBox.Show("La firma se guardo correctamente");
+            this.Close();
+        }
+
+        private void picFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics oG = Graphics.FromImage(ArchivoImagen))
+                {
+                    oG.FillEllipse(Brushes.Gray, e.X, e.Y, 5, 5);
+                }
+
+                // Actualiza la imagen del PictureBox
+                picFirma.Image = ArchivoImagen;
+            }
+        }
+    }
+}
